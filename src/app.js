32eb9d37a65f595e0277117d32398bb035508022
WALKME\tomer.l <tomer.l@walkme.com>
2018-11-18 19:37:21 +0200

    module2

--- src/app.js
+++ src/app.js
@@ -9,8 +9,15 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'});
-});
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8'});
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8'});
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
+console.log(users);
+app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts}));
+app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
+app.get('/savings', (req, res) => res.render('account', {account: accounts.savings}));
+app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
+app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
